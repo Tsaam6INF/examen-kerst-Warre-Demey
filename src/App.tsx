87d72a7b26b63913a6487a8ea03f6c999dcf2e30
Warre-Demey <Warre.demey@student.tsaam.be>
2024-12-02 10:18:10 +0100

    ik ben gemist

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react/jsx-runtime";
 
 function App() {
-  return (<Fragment><p>Info</p><h1>titel</h1></Fragment>)}
+  return (<Fragment><p>Info</p><h1>titel</h1><p>het is opgelost!</p></Fragment>)}
 export default App
